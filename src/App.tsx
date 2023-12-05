c4e27f4f7652fea61dda35d3014673a2b3d778bf
Antonio Basilio <antoniovsb25@gmail.com>
2023-12-05 11:08:15 -0300

    feat: add forward ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { useRef } from "react";
 import Button from "./components/Button";
 import Container from "./components/Container";
 import Input from "./components/Input";
 
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <main>
-      <Input id="name" label="your name" type="text" />
+      <Input id="name" label="your name" type="text" ref={inputRef} />
       <Input id="age" label="your age" type="number" />
       <Button>A Button</Button>
       <Button href="https://google.com" target="_blank">

2e1f513472af25b354b7e8427bb660b7572a7cce
Antonio Basilio <antoniovsb25@gmail.com>
2023-12-07 22:50:57 -0300

    feat: add set interval, clear interval and ref

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,34 @@
+import { useState, useEffect, useRef } from "react";
 import { type Timer } from "../store/timer-context.tsx";
 import Container from "./UI/Container.tsx";
 
 export default function Timer({ name, duration }: Timer) {
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const interval = useRef<number | null>(null);
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    const timer = (interval.current = setInterval(function () {
+      setRemainingTime((prevTime) => prevTime - 50);
+    }, 50));
+    interval.current = timer;
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
